c85cdd9a91ca6159c11b78fc2bdc996b382de9d2
Grupo Coinp Deploy <deploy@grupocoinp.com>
2026-06-28 13:05:10 -0500

    Add data dump and move media to static for Render serving

--- backend/settings.py
+++ backend/settings.py
@@ -138,5 +138,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/static/media/'
+MEDIA_ROOT = BASE_DIR / 'static' / 'media'
